d906dce1356d8673aeccd83fd9e39948f925c775
Alex Burka <aburka@seas.upenn.edu>
2017-07-14 17:14:10 -0400

    parse human ratings

--- minc_model/copy_minc_labeled_surface_images.py
+++ minc_model/copy_minc_labeled_surface_images.py
@@ -19,16 +19,12 @@ def go():
     datadir = '/x/alexburka'
     with open('ONE THOUSAND SURFACES - Surfaces.csv', 'rb') as r:
         with cd('minc_model'):
-            with open('images/props.csv', 'wb+') as w:
+            with open('images/props_w.txt', 'w+') as pw, open('images/props_h.txt', 'w+') as ph, open('images/props_r.txt', 'w+') as pr, open('images/props_ss.txt', 'w+') as pss, open('images/props_so.txt', 'w+') as pso, open('images/props_sb.txt', 'w+') as psb:
                 rdr = csv.reader(r)
-                wtr = csv.DictWriter(w, fieldnames=['ID', 'ImageStick', 'ImageOpto', 'ImageBio', 'Warmness', 'Hardness', 'Roughness', 'SlipperinessStick', 'SlipperinessOpto', 'SlipperinessBio'])
-                wtr.writeheader()
-                idx = 1
                 next(rdr)
                 for row in rdr:
                     if row[8] != '' and 'HaTT' in row[9]:
-                        props = {}
-                        for (flow, col, eff) in [('stickcam', 2, 'Stick'), ('optocam', 4, 'Opto'), ('biocam', 6, 'Bio')]:
+                        for (flow, col, ps) in [('stickcam', 2, pss), ('optocam', 4, pso), ('biocam', 6, psb)]:
                             if row[col] != '':
                                 fname = '%s/%s/%s/%s/surface.png' % (datadir, row[col], flow, row[col+1])
                                 if os.path.isfile(fname):
@@ -43,18 +39,13 @@ def go():
                                         rough = [line for line in lines if 'rough' in line]
                                         slippery = [line for line in lines if 'slippery' in line]
                                         if len(warm) == 1:
-                                            props['Warmness'] = re.split(r'[\[\]]', warm[0])[1]
+                                            pw.write('%s %s\n' % (new_fname, re.split(r'[\[\]]', warm[0])[1]))
                                         if len(hard) == 1:
-                                            props['Hardness'] = re.split(r'[\[\]]', hard[0])[1]
+                                            ph.write('%s %s\n' % (new_fname, re.split(r'[\[\]]', hard[0])[1]))
                                         if len(rough) == 1:
-                                            props['Roughness'] = re.split(r'[\[\]]', rough[0])[1]
-                                        props['Slipperiness%s' % eff] = re.split(r'[\[\]]', slippery[0])[1]
-                                        props['Image%s' % eff] = new_fname
-                        if 'SlipperinessStick' in props and 'SlipperinessOpto' in props and 'SlipperinessBio' in props:
-                            props['ID'] = idx
-                            idx = idx + 1
-                            wtr.writerow(props)
-
+                                            pr.write('%s %s\n' % (new_fname, re.split(r'[\[\]]', rough[0])[1]))
+                                        ps.write('%s %s\n' % (new_fname, re.split(r'[\[\]]', slippery[0])[1]))
+                            
 if __name__ == '__main__':
     go()
 
